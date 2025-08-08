3f007ab71102853bed901c0d4bce6e777d42caad
aziel <2021-0165@unad.edu.do>
2025-08-08 13:07:51 -0400

    Agregar archivos de proyecto.

---
+++ Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Demo
+{
+    public partial class Form1 : Form
+    {
+     
+        public Form1()
+        {
+            InitializeComponent();
+            this.label1.UseWaitCursor = false;
+            this.label2.UseWaitCursor = false;
+            this.label3.UseWaitCursor = false;
+            this.button2.UseWaitCursor = false;
+            this.button3.UseWaitCursor = false;
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "si";
+            label2.Text = "si";
+            label3.Text = "si";
+        }
+
+  
+    
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button1.Text = "NO";
+            button2.Text = "NO";
+            button3.Text = "NO";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+
+        {
+            button1.Text = "A";
+            button2.Text = "B";
+            button3.Text = "C";
+
+            label1.Text = "Hola";
+            label2.Text = "Hola";
+            label3.Text = "Hola";
+        }
+    }
+}
